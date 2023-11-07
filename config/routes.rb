191408b5ba3b862013c1bb246240cff5e93cbcd7
Serena Green <serenafgreen@gmail.com>
2023-11-06 19:39:07 -0800

    adds single route to displaceany one product

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get "/hoodie", controller: "products", action: "hoodie_info"
   get "/socks", controller: "products", action: "socks_info"
   get "/all-products", controller: "products", action: "all_products"
+  get "/any-product/:id" => "products#any_product"
 end
