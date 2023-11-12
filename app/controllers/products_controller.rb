7939bffdab521b6ee0e6d071695e5cc250614ebe
“Serena” <serenafgreen@gmail.com>
2023-11-12 13:24:10 -0800

    adds create request with post convention and action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,4 +23,17 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params["id"])
     render template: "products/show"
   end
+
+  def create
+
+  @product = Product.create(
+
+  name: params["name"],
+  price: params["price"],
+  image_url: params["image_url"],
+  description: params["description"]
+  )
+  render :show
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
 
   get "/water-bottle", controller: "products", action: "water_bottle_info"
+
   get "/hoodie", controller: "products", action: "hoodie_info"
+
   get "/socks", controller: "products", action: "socks_info"
+
   get "/all-products", controller: "products", action: "all_products"
+
   get "/any-product/:id" => "products#any_product"
+
+  post "/products" => "products#create"
+
 end
