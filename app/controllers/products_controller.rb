09223ea8e9021cf6f1a917007e161a6df70ab4ab
Serena Green <serenafgreen@gmail.com>
2023-11-12 14:36:10 -0800

    fixes update method to use defaults

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -41,10 +41,10 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params["id"])
 
     @product.update(
-      name: params["name"],
-      price: params["price"],
-      image_url: params["image_url"],
-      description: params["description"]
+      name: params["name"] || @product.name,
+      price: params["price"] || @product.price,
+      image_url: params["image_url"] || @product.image_url,
+      description: params["description"] || @product.description
     )
     render :show
   end
