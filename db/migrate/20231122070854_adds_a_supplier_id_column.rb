07c978e2f0ffab6f8017905235c2cc5ccab03ba8
“Serena” <serenafgreen@gmail.com>
2023-11-21 23:16:08 -0800

    removes column from suppliers table

--- db/migrate/20231122070854_adds_a_supplier_id_column.rb
+++
@@ -1,5 +0,0 @@
-class AddsASupplierIdColumn < ActiveRecord::Migration[7.1]
-  def change
-      add_column :suppliers, :supplier_id, :integer
-  end
-end
---
+++ db/migrate/20231122071430_removes_column_from_suppliers.rb
@@ -0,0 +1,5 @@
+class RemovesColumnFromSuppliers < ActiveRecord::Migration[7.1]
+    def change
+      remove_column :suppliers, :supplier_id, :integer
+    end
+end
