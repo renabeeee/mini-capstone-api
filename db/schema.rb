e94ebc3eab8e51cb417830668364fb34988a2bdb
Serena Green <serenafgreen@gmail.com>
2023-11-21 23:11:51 -0800

    adds column supplier_id to suppliers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_065924) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_070854) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_065924) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "supplier_id"
   end
 
 end
