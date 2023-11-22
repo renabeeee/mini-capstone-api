18b5bebdab1ad7bd7ec7f90f79da6e73f4972a12
“Serena” <serenafgreen@gmail.com>
2023-11-21 23:17:54 -0800

    adds supplier_id column to products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_071430) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_071633) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_071430) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "supplier_id"
   end
 
   create_table "suppliers", force: :cascade do |t|
