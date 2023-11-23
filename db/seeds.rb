82164131a5f86ca9d775a162c8e663e12566f780
“Serena” <serenafgreen@gmail.com>
2023-11-22 20:39:03 -0800

    creates views for suppliers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,5 @@
-Product.create!([
-  {name: "Water Bottle", price: "14.0", image_url: "https://yeti-web.imgix.net/41f88948c3f994ba/W-Drinkware_BottleChug_64oz_Black_Studio_PrimaryB.png?bg=0fff&auto=format&w=846&h=846", description: "64 oz water bottle with chug cap"},
-  {name: "Hoodie", price: "48.0", image_url: "https://cdn.shopify.com/s/files/1/2185/2813/files/W3550RG_01_b2_s1_a1_m18_750x.jpg?v=1699463916", description: "Lululemon Cotton Hoodie"},
-  {name: "Sweatshirt", price: "40.0", image_url: "https://cdn.shopify.com/s/files/1/2185/2813/files/U3031RG_02910_b1_s1_a1_1_m18_4f6c17b5-1fc3-4024-8ecb-ac86b648c302_750x.jpg?v=1698274544", description: "Heavy cotton sweatshirt"},
-  {name: "Beanie", price: "20.0", image_url: "https://cdn.shopify.com/s/files/1/2185/2813/products/A024U_02910_1_750x.jpg?v=1639597471", description: "Handknit beanie"},
-  {name: "Athletic socks", price: "10.0", image_url: "https://cdn.shopify.com/s/files/1/2185/2813/products/A0480U_0001_b1_a1_750x.jpg?v=1669748933", description: "White throwback barre socks"}
-])
+supplier = Supplier.create!(name: "The Marathon Clothing", email: "service@themarathonclothing.com", phone_number: "562-296-1818")
 
-supplier = Supplier.new(name: "The Marathon Clothing", email: "service@themarathonclothing.com", phone_number: "562-296-1818")
-supplier.save!
+supplier = Supplier.create!(name: "Girls Who Dress Like Boys", email: "GWDLBofficial@gmail.com", phone_number: "343-299-4750")
 
-supplier = Supplier.new(name: "Girls Who Dress Like Boys", email: "GWDLBofficial@gmail.com", phone_number: "343-299-4750")
-supplier.save!
-
-supplier = Supplier.new(name: "Adidas", email: "info@adidas.com", phone_number: "290-433-6878")
-supplier.save!
+supplier = Supplier.create!(name: "Adidas", email: "info@adidas.com", phone_number: "290-433-6878")
