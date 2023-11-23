08ce5beaf632d9cbf6f138a5f5b35b3bcd80228b
Serena Green <serenafgreen@gmail.com>
2023-11-22 20:17:47 -0800

    updates products table with supplier_id

--- db/migrate/20231122071430_removes_column_from_suppliers.rb
+++ db/migrate/20231122071430_removes_column_from_suppliers.rb
@@ -1,5 +1,5 @@
-class RemovesColumnFromSuppliers < ActiveRecord::Migration[7.1]
-    def change
-      remove_column :suppliers, :supplier_id, :integer
-    end
-end
+# class RemovesColumnFromSuppliers < ActiveRecord::Migration[7.1]
+#     def change
+#       remove_column :suppliers, :supplier_id, :integer
+#     end
+# end
