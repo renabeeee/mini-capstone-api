7e17baebaf6eb5a5b4102e8f9423845f509a3197
Serena Green <serenafgreen@gmail.com>
2023-12-02 00:36:08 -0800

    updates create action to calculate tax, subtotal, and total for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,23 @@
 class OrdersController < ApplicationController
 
   def create
+    product = Product.find_by(id: params[:product_id])
+
+    calculated_subtotal = products * params[:quantity].to_i
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total =  calculated_subtotal + calculated_tax
+
+
   @order = Order.new(
-    user_id: params[:user_id],
+    user_id: current_user.id,
     product_id: params[:product_id],
     quantity: params[:quantity],
-    subtotal: params[:subtotal],
-    tax: params[:tax],
-    total: params[:total],
+    subtotal: subtotal,
+    tax: tax,
+    total: total,
   )
   @order.save
-  render json: @order.as_json
+  render :show
   end
 
   def show
