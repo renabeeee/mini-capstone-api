4d92856987c2c138dfa649c97b2e19ff163d7763
“Serena” <serenafgreen@gmail.com>
2023-12-02 15:34:33 -0800

    adds admin column to users in user db

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,24 +1,26 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user
 
   def create
-    product = Product.find_by(id: params[:product_id])
+      product = Product.find_by(id: params[:product_id])
 
-    calculated_subtotal = products * params[:quantity].to_i
-    calculated_tax = calculated_subtotal * 0.09
-    calculated_total =  calculated_subtotal + calculated_tax
+      calculated_subtotal = product.price * params[:quantity].to_i
+      calculated_tax = calculated_subtotal * 0.09
+      calculated_total =  calculated_subtotal + calculated_tax
 
 
-  @order = Order.new(
-    user_id: current_user.id,
-    product_id: params[:product_id],
-    quantity: params[:quantity],
-    subtotal: subtotal,
-    tax: tax,
-    total: total,
-  )
-  @order.save
-  render :show
+      @order = Order.new(
+        user_id: current_user.id,
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        subtotal: calculated_subtotal,
+        tax: calculated_tax,
+        total: calculated_total,
+      )
+      @order.save
+      render :show
   end
+
 
   def show
     @order = current_user.orders.find_by(id: params[:id])
@@ -26,7 +28,7 @@ class OrdersController < ApplicationController
   end
 
   def index
-    @orders = current_user.orders
-    render template: "orders/index"
+      @orders = current_user.orders
+      render template: "orders/index"
   end
 end
