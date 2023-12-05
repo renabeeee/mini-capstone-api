becd38e7bfaf1c89fd254ba0bb916831eb921268
Serena Green <serenafgreen@gmail.com>
2023-12-04 18:55:54 -0800

    updates view and tests for categories

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -19,6 +19,23 @@ test "create" do
     assert_response 401
   end
 
+  test "index" do
+    get "/products.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Product.count, data.length
+  end
+
+  test "show" do
+    get "/products/#{Product.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "name", "price", "categories", "supplier", "is_discounted", "tax", "total", "images", "description", "quantity", "created_at", "updated_at"],
+    data.keys
+  end
+
   test "update" do
     product = Product.first
     patch "/products/#{product.id}.json",
