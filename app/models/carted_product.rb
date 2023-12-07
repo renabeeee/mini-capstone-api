d6c6a2c04c211478e7146dd068c21a83d12dd7a0
Serena Green <serenafgreen@gmail.com>
2023-12-07 15:36:04 -0800

    adds associations to models

--- app/models/carted_product.rb
+++ app/models/carted_product.rb
@@ -1,2 +1,5 @@
 class CartedProduct < ApplicationRecord
+  belongs_to :user
+  belongs_to :product_id
+  belongs_to :order, optional: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,12 @@
 class Product < ApplicationRecord
-  has_many :orders
+  has_many :carted_products
+  has_many :orders, through :carted_products
   belongs_to :supplier
-  has_many :orders
-
   has_many :category_products
   has_many :categories, through: :category_products
 
   validates :price, presence: true
   validates :price, numericality: { greater_than: 0}
-
 
   def supplier
     Supplier.find_by(id: supplier_id)
