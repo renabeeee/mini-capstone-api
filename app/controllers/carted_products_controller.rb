069b80efeeeedaf615f4177deaf751e9817f7157
“Serena” <serenafgreen@gmail.com>
2023-12-07 20:41:12 -0800

    uses jbuilder shortcuts

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,2 +1,16 @@
 class CartedProductsController < ApplicationController
+
+  def create
+        @carted_product = CartedProduct.new(
+        user_id: params[:user_id],
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        status: "carted",
+      )
+      if @carted_product.save
+        render :show
+      else
+        render json: { errors: @carted_product.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
--- app/models/carted_product.rb
+++ app/models/carted_product.rb
@@ -1,5 +1,5 @@
 class CartedProduct < ApplicationRecord
-  belongs_to :user
-  belongs_to :product_id
+  belongs_to :product
   belongs_to :order, optional: true
+  belongs_to :user
 end
